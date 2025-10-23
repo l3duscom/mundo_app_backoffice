17d72a29d7c7897df415f69b3e737f9d528268fe
Eduardo Santos <eduardosantos_777@icloud.com>
2025-10-22 21:05:20 -0300

    card_step_1

--- app/Views/Checkout/cartao_step_1 copy.php
+++ app/Views/Checkout/cartao_step_1 copy.php
@@ -141,7 +141,122 @@
 
 <?php echo $this->section('scripts') ?>
 
+<script src="<?php echo site_url('recursos/vendor/loadingoverlay/loadingoverlay.min.js') ?>"></script>
 
+
+<script src="<?php echo site_url('recursos/vendor/mask/jquery.mask.min.js') ?>"></script>
+<script src="<?php echo site_url('recursos/vendor/mask/app.js') ?>"></script>
+
+
+<script>
+    $(document).ready(function() {
+
+        //$("#form").LoadingOverlay("show");
+
+
+
+
+        $("#form").on('submit', function(e) {
+
+
+            e.preventDefault();
+
+
+            $.ajax({
+
+                type: 'POST',
+                url: '<?php echo site_url('checkout/cartao_step_2'); ?>',
+                data: new FormData(this),
+                dataType: 'json',
+                contentType: false,
+                cache: false,
+                processData: false,
+                beforeSend: function() {
+
+                    $("#response").html('');
+                    $("#btn-salvar").val('Por favor aguarde...');
+
+                },
+                success: function(response) {
+
+                    $("#btn-salvar").val('Salvar');
+                    $("#btn-salvar").removeAttr("disabled");
+
+                    $('[name=csrf_ordem]').val(response.token);
+
+
+                    if (!response.erro) {
+
+
+                        if (response.info) {
+
+                            $("#response").html('<div class="alert alert-info">' + response
+                                .info + '</div>');
+
+                        } else {
+
+                            // Tudo certo com a atualização do usuário
+                            // Podemos agora redirecioná-lo tranquilamente
+
+                            window.location.href =
+                                "<?php echo site_url("checkout/qrcode/"); ?>" + response.id;
+
+                        }
+
+                    }
+
+                    if (response.erro) {
+
+                        // Exitem erros de validação
+
+
+                        $("#response").html('<div class="alert alert-danger">' + response.erro +
+                            '</div>');
+
+
+                        if (response.erros_model) {
+
+
+                            $.each(response.erros_model, function(key, value) {
+
+                                $("#response").append(
+                                    '<ul class="list-unstyled"><li class="text-danger">' +
+                                    value + '</li></ul>');
+
+                            });
+
+                        }
+
+                    }
+
+                },
+                error: function() {
+
+                    alert(
+                        'Não foi possível procesar a solicitação. Por favor entre em contato com o suporte técnico.'
+                    );
+                    $("#btn-salvar").val('Salvar');
+                    $("#btn-salvar").removeAttr("disabled");
+
+                }
+
+
+
+            });
+
+
+        });
+
+
+        $("#form").submit(function() {
+
+            $(this).find(":submit").attr('disabled', 'disabled');
+
+        });
+
+
+    });
+</script>
 
 
 <?php echo $this->endSection() ?>
