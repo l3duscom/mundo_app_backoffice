61447f4d6fd775ff803d3836d9a881228aab39f7
Eduardo Santos <eduardosantos_777@icloud.com>
2025-11-12 09:13:37 -0300

    carrinho

--- app/Views/Carrinho/index.php
+++ app/Views/Carrinho/index.php
@@ -772,8 +772,6 @@ if (isset($event_id)) {
                             <div class="mb-0 mt-3 font-24" style="color: #333;">Selecione seu ingresso </div>
                             <p>Apenas a promoção de maior desconto será aplicada ao final do carrinho.</p>
 
-                            <h2>Para adquirir as opções PLATEIA (Domingo) e EPIC PASS, navegue no menu acima</h2
-
                             <?php foreach ($items as $key => $value) : ?>
                                 <?php if (($value['categoria'] == 'epic' && empty($value['parent_ticket_id']))) : ?>
                                     <div class="card border border-muted px-3" data-item-id="<?= $key ?>">
